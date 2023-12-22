96e3b89a2dc1a3bbb4589217520288ac700d7cf9
Inderpal Singh <92572473+99inder@users.noreply.github.com>
2023-12-22 20:20:23 +0530

    Added framer motion

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useRef, useState } from 'react';
 import data from '../assets/uavsData';
+import { motion } from 'framer-motion';
 
 const ProductPage = () => {
     const [selectedItemData, setSelectedItemData] = useState(data[0]);
-    const { category, name, specifications, imgSrc, audioSrc } = selectedItemData;
+    const { id, category, name, specifications, imgSrc, audioSrc } = selectedItemData;
 
     const audioRef = useRef();
     const sourceRef = useRef();
+
+    useEffect(() => {
+        // To fix the on load scroll issue with framer motion
+        scrollTo({ top: 0, left: 0, behavior: 'instant' });
+    }, []);
 
     function formatString(input) {
         // Replace underscores with spaces
@@ -30,23 +36,65 @@ const ProductPage = () => {
 
     return (
         <div className='flex flex-col justify-between lg:flex-row gap-16 lg:items-center'>
-            <div className='flex flex-col gap-6 lg:w-2/4'>
-                <img src={imgSrc} alt={name} className='w-full h-full aspect-square object-cover rounded-xl' />
-                <div className='flex flex-row justify-center gap-x-3 h-24'>
+            <motion.div
+                initial={{
+                    scale: 0,
+                    opacity: 0.5
+                }}
+                animate={{
+                    scale: 1,
+                    opacity: 1
+                }}
+                transition={{
+                    duration: 0.5,
+                }}
+                className='flex flex-col gap-6 lg:w-2/4'
+            >
+                <img
+                    src={imgSrc} alt={name} className='w-full h-full aspect-square object-cover rounded-xl shadow-neutral-500 shadow-lg drop-shadow-2xl'
+                />
+                <motion.div
+                    initial={{
+                        scale: 0,
+                        opacity: 0.5
+                    }}
+                    animate={{
+                        scale: 1,
+                        opacity: 1
+                    }}
+                    transition={{
+                        duration: 0.2,
+                        delay: 0.5
+                    }}
+                    className='flex flex-row justify-center gap-x-3 h-24'
+                >
                     {data.map((item, index) => (
                         <img
                             key={index}
                             src={item.imgSrc}
                             alt={"img-" + item.id}
-                            className='w-24 h-24 rounded-md cursor-pointer'
+                            className={`w-24 h-24 rounded-md cursor-pointer duration-200 shadow-sky-200 shadow-lg scale-110 ${id !== item.id && "blur-[2px] shadow-none hover:blur-none scale-95 hover:scale-100"} transition-all duration-200`}
                             onClick={() => setSelectedItemData(item)}
                         />
                     ))}
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
 
             {/* DETAILS */}
-            <div className='flex flex-col gap-4 lg:w-2/4'>
+            <motion.div
+                initial={{
+                    opacity: 0,
+                    x: '50%'
+                }}
+                animate={{
+                    opacity: 1,
+                    x: 0
+                }}
+                transition={{
+                    duration: 0.5,
+                }}
+                className='flex flex-col gap-4 lg:w-2/4'
+            >
                 <div>
                     <span className='text-violet-600 font-semibold'>{category}</span>
                     <h1 className='text-3xl font-bold'>{name}</h1>
@@ -69,7 +117,7 @@ const ProductPage = () => {
                         </ul>
                     )}
                 </div>
-            </div>
+            </motion.div>
         </div>
     );
 };
